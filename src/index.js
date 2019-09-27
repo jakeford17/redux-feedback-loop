961a07eaa6181691516874284ff68f873cdb189d
Jake Ford <jakemford17@gmail.com>
2019-09-27 17:59:29 -0500

    Added reducer to index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,27 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 
+const feedbackReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_FEELINGS': 
+        state = [...state, action.payload]
+        return state
+        case 'SET_UNDERSTANDING': 
+        state = [...state, action.payload]
+        return state
+        case 'SET_SUPPORTED': 
+        state = [...state, action.payload]
+        return state
+        case 'SET_COMMENTS': 
+        state = [...state, action.payload]
+        return state
+        default: return state
+    }
+}
+
 const reduxStore = createStore(
     combineReducers({
-        // list all reducers
+        feedbackReducer
     }),
     applyMiddleware(logger)
 );
