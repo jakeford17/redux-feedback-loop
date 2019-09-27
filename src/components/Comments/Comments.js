767790d46d13e25844577de080d3c8ee2b390a78
Jake Ford <jakemford17@gmail.com>
2019-09-27 18:43:57 -0500

    Added to Comments Component

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+
 
 class Comments extends Component {
+    state = {
+        comments: '',
+        button: true
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            comments: event.target.value,
+            button: false
+        })
+    }
+
+    handleClick = (event) => {
+        this.props.dispatch({type: 'SET_COMMENTS', payload: this.state.comments})
+        console.log('From comments: ', this.state.comments)
+    }
 
     render() {
         return (
-            <>
-            </>
+            <Router>
+            <h2>Any additional comments?</h2>
+            <div>
+            <input onChange={this.handleChange} value={this.state.comments}/>
+            </div>
+            <br/>
+            <Link to="/review"><button onClick={this.handleClick} disabled={this.state.button} >Next</button></Link>
+            </Router>
         );
     }
 }
