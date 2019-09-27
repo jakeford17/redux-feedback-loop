f8d3645e72e0897877ee377afde631675853e4c9
Jake Ford <jakemford17@gmail.com>
2019-09-27 17:37:34 -0500

    Added feedback.router.js to server.js

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.post('/',  (req, res) => {
+    let newFeedback = req.body;
+    console.log(`Adding FEEDBACK: `, newFeedback);
+    let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") 
+                    VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [newFeedback.feeling, newFeedback.understanding, newFeedback.support, newFeedback.comments])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new task`, error);
+        res.sendStatus(500);
+      });
+  });
+
+module.exports = router;
