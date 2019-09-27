ce8bccfacb05316e5f9cbd7e2144bfa33d117296
Jake Ford <jakemford17@gmail.com>
2019-09-27 18:34:04 -0500

    Added to Understanding and Supported Components

--- src/components/Supported/Supported.js
+++ src/components/Supported/Supported.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 class Supported extends Component {
+    state = {
+        support: '1'
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            support: event.target.value
+        })
+    }
+
+    handleClick = (event) => {
+        this.props.dispatch({type: 'SET_SUPPORTED', payload: this.state.support})
+        console.log('From support: ', this.state.support)
+    }
 
     render() {
         return (
-            <>
-            </>
+            <Router>
+            <h2>How was your instructor support today?</h2>
+            <h3>(1 is "I might as well just learn this on my own" and 5 is "I couldn't be more supported")</h3>
+            <div>
+            <select onChange={this.handleChange} value={this.state.feeling}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            </div>
+            <br/>
+            <Link to="/4"><button onClick={this.handleClick}>Next</button></Link>
+            </Router>
         );
     }
 }
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 class Understanding extends Component {
 
+    state = {
+        understanding: '1'
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            understanding: event.target.value
+        })
+    }
+
+    handleClick = (event) => {
+        this.props.dispatch({type: 'SET_UNDERSTANDING', payload: this.state.understanding})
+        console.log('From understanding: ', this.state.understanding)
+    }
+
     render() {
         return (
-            <>
-            </>
+            <Router>
+            <h2>How well do you understand today's material?</h2>
+            <h3>(1 is "I'm so lost!" and 5 is "I'm an expert!")</h3>
+            <div>
+            <select onChange={this.handleChange} value={this.state.understanding}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            </div>
+            <br/>
+            <Link to="/3"><button onClick={this.handleClick}>Next</button></Link>
+            </Router>
         );
     }
 }
