55f750c9b6bdb361c290424f366b9f36b354110b
Jake Ford <jakemford17@gmail.com>
2019-09-27 18:59:21 -0500

    Added to Success and redirect to home

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+
 
 class Success extends Component {
 
+    handleClick = (event) => {
+        console.log("SUBMIT CLICKED");
+    }
+
     render() {
         return (
-            <>
-            </>
+            <Router>
+            <h1>THANKS FOR YOUR FEEDBACK!</h1>
+            <Link to="/"><button onClick={this.handleClick}>SUBMIT!</button></Link>
+            </Router>
         );
     }
 }
